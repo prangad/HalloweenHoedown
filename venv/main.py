4f7a130ec40564e7dffbe5a29799178ac972063e
aaron <kopplina@mail.gvsu.edu>
2020-11-13 22:04:25 -0500

    adding fight menu and main window

--- venv/main.py
+++ venv/main.py
@@ -1,19 +1,39 @@
 import pygame
+import State
+import FightMenu
 
-def main():
-    pygame.init()
-    window = pygame.display.set_mode([800, 800])
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+class Window:
+    def __init__(self, neighborhood, fight_menu):
+        pygame.init()
+        self.window = pygame.display.set_mode([800, 800])
+        self.running = True
+        self.state = State.MAP
+        self.map = neighborhood
+        self.fight_menu = fight_menu
+        self.game_loop()
 
-        window.fill((150, 150, 150))
-        pygame.draw.circle(window, (255, 0, 0), (400, 400), 100)
-        pygame.display.flip()
+    def game_loop(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
 
-    pygame.quit()
+            if self.state == State.FIGHT_MENU:
+                for object in self.fight_menu.get_objects():
+                    print("drawing fight menu object")
+            elif self.state == State.MAP:
+                for object in self.map.get_objects():
+                    print("drawing map object")
+            # self.window.fill((150, 150, 150))
+            # pygame.draw.circle(self.window, (255, 0, 0), (400, 400), 100)
+            pygame.display.flip()
+
+        pygame.quit()
+
 
 if __name__ == "__main__":
-    main()
+    person = None
+    enemy = None
+    fight_menu = FightMenu.FightMenu(person, enemy)
+    # neighborhood = Map.Map()
+    game = Window(None, fight_menu)
